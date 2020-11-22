ddb107f3e2cf0c6a307dd301f56a4015f66c2e78
paung23 <paung@fandm.edu>
2020-11-22 18:39:14 -0500

    Incorporated http_request function

--- src/ProxyServer.cpp
+++ src/ProxyServer.cpp
@@ -4,13 +4,97 @@
 #include <iostream>
 #include <string>
 
+#include <istream>
+#include <ostream>
+
 using asio::ip::tcp;
+
+int http_request(std::string get_string, std::string host_string) {
+    try {
+        asio::io_service io_service;
+        asio::error_code error;
+
+        // Get a list of endpoints corresponding to the server name.
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(host_string,  "http");
+        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+        tcp::resolver::iterator end;
+
+        // Try each endpoint until we successfully establish a connection.
+        tcp::socket socket(io_service);
+        while (endpoint_iterator != end) {
+            socket.close();
+            socket.connect(*endpoint_iterator++, error);
+        }
+
+        // Form the request. We specify the "Connection: close" header so that the
+        // server will close the socket after transmitting the response. This will
+        // allow us to treat all data up until the EOF as the content.
+        asio::streambuf request;
+        std::ostream request_stream(&request);
+        request_stream << "GET " << get_string << " HTTP/1.0\r\n";
+        request_stream << "Host: " << host_string << "\r\n";
+        request_stream << "Accept: */*\r\n";
+        request_stream << "Connection: close\r\n\r\n";
+
+        // Send the request.
+        asio::write(socket, request);
+
+        // Read the response status line. The response streambuf will automatically
+        // grow to accommodate the entire line. The growth may be limited by passing
+        // a maximum size to the streambuf constructor.
+        asio::streambuf response;
+        asio::read_until(socket, response, "\r\n");
+
+        // Check that response is OK.
+        std::istream response_stream(&response);
+        std::string http_version;
+        response_stream >> http_version;
+        unsigned int status_code;
+        response_stream >> status_code;
+        std::string status_message;
+        std::getline(response_stream, status_message);
+        if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+            std::cout << "Invalid response\n";
+            return 1;
+        }
+        if (status_code != 200) {
+            std::cout << "Response returned with status code " << status_code << "\n";
+            return 1;
+        }
+
+        // Read the response headers, which are terminated by a blank line.
+        asio::read_until(socket, response, "\r\n\r\n");
+
+        // Process the response headers.
+        std::string header;
+        while (std::getline(response_stream, header) && header != "\r")
+            std::cout << header << "\n";
+        std::cout << "\n";
+
+        // Write whatever content we already have to output.
+        if (response.size() > 0)
+            std::cout << &response;
+
+        // Read until EOF, writing data to output as we go.
+        while (asio::read(socket, response, asio::transfer_at_least(1), error))
+            std::cout << &response;
+        if (error != asio::error::eof)
+            throw error;
+    } catch (std::exception& e) {
+        std::cout << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
 
 int main() {
     asio::io_context io_context;
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8001));
 
     while (true) {
+        std::string request = "";
+
         // Wait for client
         std::cout << ">>> Waiting for incoming requests..." << std::endl;
         tcp::socket socket(io_context);
@@ -26,8 +110,22 @@ int main() {
 
         for (int i = 0; i < len; i++) {
             auto x = *reinterpret_cast<char*>(&buf.data()[i]);
-            std::cout << x;
+            //std::cout << x;
+
+            request += x;
         }
+
+        int get_begin = request.find("GET ")+4;
+        int get_end = request.find("HTTP/1.0")-5; //Only for Firefox on Phyo's laptop
+
+        std::string get_string = request.substr(get_begin, get_end);
+
+        int host_begin = request.find("Host: ")+6;
+        int host_end = request.find("User-Agent:")-44; //Only for Firefox on Phyo's laptop
+
+        std::string host_string = request.substr(host_begin, host_end);
+
+        http_request(get_string, host_string);
     }
 
     return 0;
