04117ee51ccff26d6f5c4d188958e4ab6a1c52c4
paung23 <paung@fandm.edu>
2020-11-29 19:43:51 -0500

    Added error handling for possible scenarios. Added comments.

--- src/ProxyServer.cpp
+++ src/ProxyServer.cpp
@@ -1,3 +1,9 @@
+/**
+ * Austin Butz and Phyo Thuta Aung
+ * CPS 373: Intro to Computer Networking
+ * Final Project: "PROXY SERVER"
+ */
+
 #include <inttypes.h>
 #include <asio.hpp>
 #include <ctime>
@@ -11,8 +17,16 @@
 
 using asio::ip::tcp;
 
-std::string makeCustomHTTPresponse(int status_code, const std::string& status_message, const std::string& explanation)
+/**
+ * A function to make a custom HTTP response (for error handling)
+ * @param status_code
+ * @param status_message
+ * @param explanation
+ * @return custom HTTP page
+ */
+std::string makeCustomHTTPresponse(unsigned int status_code, const std::string& status_message, const std::string& explanation)
 {
+    // Prepare a custom HTTP page with CSS (based on index.html from example.com)
     std::string content = "<!doctype html>\n"
                           "<html>\n"
                           "<head>\n"
@@ -48,16 +62,24 @@ std::string makeCustomHTTPresponse(int status_code, const std::string& status_me
                           "</body>\n"
                           "</html>";
 
-    std::string output = "HTTP/1.0 " + std::to_string(status_code) + " " + status_message + "\n";
-    output += "Content-Type: text/html; charset=UTF-8\n";
-    output += "Content-Length: "+std::to_string(content.size())+"\n";
-    output += "Connection: close\n\n";
+    // Prepare HTTP headers
+    std::string output = "HTTP/1.0 " + std::to_string(status_code) + " " + status_message + "\r\n";
+    output += "Content-Type: text/html; charset=UTF-8\r\n";
+    output += "Content-Length: "+std::to_string(content.size())+"\r\n";
+    output += "Connection: close\r\n\r\n";
     output += content;
 
     return output;
 }
 
+/**
+ * A function to make a HTTP request using Asio library (based on Professor McDanel's code)
+ * @param get_string
+ * @param host_string
+ * @return vector of HTTP responses
+ */
 std::vector<std::string> http_request(std::string get_string, std::string host_string) {
+    std::vector<std::string> responses;
     try {
         asio::io_service io_service;
         asio::error_code error;
@@ -88,39 +110,16 @@ std::vector<std::string> http_request(std::string get_string, std::string host_s
         // Send the request.
         asio::write(socket, request);
 
-        /**
-         * Reading the whole HTTP response directly --> works for some images
-         *
-        std::string http_response = ""; // Phyo: Formation of HTTP response
-
-        //long packet_size = 0; // Phyo: Packet Size
-
-        asio::streambuf response;
-        asio::read(socket, response, error);
-        std::istream response_stream(&response);
-        std::string line;
-        std::string output;
-        while (std::getline(response_stream, line)) {
-            //std::cout << line << std::endl;
-            output += line+"\n";
-        }
-        if (error != asio::error::eof)
-            throw error;
-
-        return output;
-         */
-
-        std::vector<std::string> responses;
         long packet_size = 0; // Phyo: Packet Size
 
-        // Read the response status line. The response streambuf will automatically
-        // grow to accommodate the entire line. The growth may be limited by passing
-        // a maximum size to the streambuf constructor.
-        asio::streambuf response(1500);
-
+        // For each HTTP response, read it from the socket, analyze for errors, form the output and push it to "responses" vector
         do {
             std::string http_response = ""; // Phyo: Formation of HTTP response
 
+            // Read the response status line. The response streambuf will automatically
+            // grow to accommodate the entire line. The growth may be limited by passing
+            // a maximum size to the streambuf constructor.
+            asio::streambuf response;
             asio::read_until(socket, response, "\r\n"); // 1st line
 
             // Check that response is OK.
@@ -133,13 +132,15 @@ std::vector<std::string> http_request(std::string get_string, std::string host_s
             std::getline(response_stream, status_message);
 
             if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
-                responses.push_back("Invalid response\n");
+                responses.push_back(makeCustomHTTPresponse(400, "Bad Request", "The server did not understand the request."));
+                return responses;
             }
             if (status_code != 200) {
                 responses.push_back(makeCustomHTTPresponse(status_code, status_message.c_str(), "Oops! There are some issues. Please keep in mind that our proxy server can only support HTTP protocol!"));
+                return responses;
             }
 
-            http_response += "HTTP/1.0 200 OK\n"; // Phyo: Formation of HTTP response
+            http_response += "HTTP/1.0 200 OK\r\n"; // Phyo: Formation of HTTP response
 
             // Read the response headers, which are terminated by a blank line.
             asio::read_until(socket, response, "\r\n\r\n");
@@ -153,12 +154,12 @@ std::vector<std::string> http_request(std::string get_string, std::string host_s
                     std::string packet_size_str = header.substr(start, end - start);
                     packet_size = std::stol(packet_size_str);
 
-                    std::cout << "* Content-Length: " << packet_size << std::endl;
+                    //std::cout << "* Content-Length: " << packet_size << std::endl;
                 }
 
-                http_response += header + "\n"; // Phyo: Formation of HTTP response
+                http_response += header + "\r\n"; // Phyo: Formation of HTTP response
             }
-            http_response += "\n"; // Phyo: Formation of HTTP response
+            http_response += "\r\n"; // Phyo: Formation of HTTP response
 
             std::string body;
             bool first = true;
@@ -194,35 +195,59 @@ std::vector<std::string> http_request(std::string get_string, std::string host_s
         } while (packet_size > 1500);
 
         return responses;
+
+        /**
+         * Another approach: Reading the whole HTTP response directly --> works for some images
+         *
+        std::string http_response = ""; // Phyo: Formation of HTTP response
+        asio::streambuf response;
+        asio::read(socket, response, error);
+        std::istream response_stream(&response);
+        std::string line;
+        std::string output;
+        while (std::getline(response_stream, line)) {
+            //std::cout << line << std::endl;
+            output += line+"\n";
+        }
+        if (error != asio::error::eof)
+            throw error;
+        return output;
+         */
     }
     catch (std::exception& e) {
         std::cout << "Exception: " << e.what() << "\n";
     }
 
-    //return "Something went wrong!";
-    return std::vector<std::string>();
+    responses.push_back(makeCustomHTTPresponse(400, "Bad Request", "The server did not understand the request."));
+    return responses;
 }
 
 int main(int argc, char* argv[]) {
     int PORT = 8001;
+    std::vector<std::string> blacklist;
+
+    // Default port will be 8001 unless specified otherwise
     if (argc > 1) {
         PORT = atoi(argv[1]);
     }
 
-    std::vector<std::string> blacklist;
-    blacklist.push_back("www.archives.nd.edu");
+    // Prepare a list of blacklisted websites for content control
+    std::ifstream infile("../data/blacklist.txt");
+    std::string line;
+    while (std::getline(infile, line)) {
+        if (!line.empty()) {
+            blacklist.push_back(line);
+        }
+    }
 
-    std::ofstream outfile;
-    outfile.open("../output.txt");
-
-    time_t now = time(0);
-    char* dt = ctime(&now);
+    // Log the network traffic in a file called log.txt from the /data folder
+    std::ofstream outfile("../data/log.txt");
 
     asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8001));
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
 
     while (true) {
-        std::string request = "";
+        std::string client_request = "";
 
         // Wait for client
         std::cout << ">>> Listening to incoming requests from Firefox at port " << PORT << "..." << std::endl;
@@ -233,52 +258,69 @@ int main(int argc, char* argv[]) {
         asio::error_code error;
         size_t len = socket.read_some(asio::buffer(buf), error);
 
+        time_t now = time(0);
+        char* dt = ctime(&now);
+
         for (int i = 0; i < len; i++) {
             auto x = *reinterpret_cast<char*>(&buf.data()[i]);
-            request += x;
+            client_request += x;
         }
 
-        std::string::size_type get_begin = request.find("GET ") + 4;
-        std::string::size_type get_end = request.find("\n", get_begin) - 10;
-        std::string get_string = request.substr(get_begin, get_end - get_begin);
+        // Parsing GET and host from the request
+        std::string::size_type get_begin = client_request.find("GET ") + 4;
+        std::string::size_type get_end = client_request.find("\n", get_begin) - 10;
+        std::string get_string = client_request.substr(get_begin, get_end - get_begin);
 
-        std::string::size_type host_begin = request.find("Host: ") + 6;
-        std::string::size_type host_end = request.find("User-Agent:", host_begin) - 2;
-        std::string host_string = request.substr(host_begin, host_end - host_begin);
+        std::string::size_type host_begin = client_request.find("Host: ") + 6;
+        std::string::size_type host_end = client_request.find("User-Agent:", host_begin) - 2;
+        std::string host_string = client_request.substr(host_begin, host_end - host_begin);
 
-        if (get_string.find("www.") == std::string::npos) {
-            get_string.insert(7, "www.");
+        // If the request is not using HTTP protocol, make a custom HTTP error response
+        if (get_string.find("http://") == std::string::npos) {
+            asio::write(socket, asio::buffer(makeCustomHTTPresponse(400, "Bad Request", "Our proxy server can only support HTTP protocol!")), error);
+            std::cout << "- Bad Request" << std::endl;
         }
-
-        if (host_string.find("www.") == std::string::npos) {
-            host_string = "www." + host_string;
-        }
-
-        outfile << "Get: " << get_string << std::endl;
-        outfile << "Host: " << host_string << std::endl;
-        outfile << "Date: " << dt << std::endl;
-
-        bool blacklist_check = false;
-        for (size_t i = 0; i < blacklist.size(); i++) {
-            if (blacklist[i] == host_string) blacklist_check = true;
-        }
-
-        if (blacklist_check == false) {
-            std::vector<std::string> responses;
-            responses = http_request(get_string, host_string);
-
-            for (int i = 0; i < responses.size(); i++) {
-                asio::write(socket, asio::buffer(responses[i]), error);
-
-                //std::cout << "----------------------" << std::endl;
-                //std::cout << responses[i] << std::endl;
-                //std::cout << "----------------------" << std::endl;
+        else {
+            // Add "www." if it is not there already
+            if (get_string.find("www.") == std::string::npos) {
+                get_string.insert(7, "www.");
             }
 
-            std::cout << std::endl;
+            if (host_string.find("www.") == std::string::npos) {
+                host_string = "www." + host_string;
+            }
+
+            // Log the request
+            outfile << "GET " << get_string << std::endl;
+            outfile << "Host: " << host_string << std::endl;
+            outfile << "Timestamp: " << dt << std::endl;
+
+            // Do a check with the blacklisted sites
+            bool blacklist_check = false;
+            for (size_t i = 0; i < blacklist.size(); i++) {
+                if (blacklist[i] == host_string) blacklist_check = true;
+            }
+
+            if (blacklist_check == false) {
+                std::vector<std::string> responses;
+                responses = http_request(get_string, host_string);
+
+                for (int i = 0; i < responses.size(); i++) {
+                    asio::write(socket, asio::buffer(responses[i]), error);
+                    std::cout << "- Request processed." << std::endl;
+                }
+            }
+            else {
+                asio::write(socket, asio::buffer(makeCustomHTTPresponse(403, "Forbidden", "Access is forbidden to the requested page.")), error);
+                std::cout << "- Forbidden request" << std::endl;
+            }
         }
 
-        else std::cout << "Invalid request, this domain is blacklisted" << std::endl;
+        std::cout << std::endl;
     }
+
+    infile.close();
+    outfile.close();
+
     return 0;
 }
